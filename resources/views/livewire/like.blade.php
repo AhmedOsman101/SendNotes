22c7cae6d605acb5e4baee21058253ff4b2f1fd8
AhmadOthman <trollingosman101@outlook.com>
2024-07-17 17:08:59 +0300

    update like.blade.php

--- resources/views/livewire/like.blade.php
+++ resources/views/livewire/like.blade.php
@@ -7,21 +7,23 @@ new class extends Component {
     public Note $note;
     public int $heartsCount;
 
-    public function mount(Note $note) {
+    public function mount(Note $note)
+    {
         $this->note = $note;
         $this->heartsCount = $note->hearts_count;
     }
 
-    public function like() {
+    public function like()
+    {
         $this->note->increment('hearts_count');
         $this->heartsCount = $this->note->hearts_count;
     }
-}
+};
 
 ?>
 
 <div>
-    <x-button xs wire:click='like' rose icon="heart" spinner>
-        <span x-text="{{$heartsCount}}"></span>
+    <x-button icon="heart" rose spinner wire:click='like' xs>
+        <span x-text="$wire.heartsCount"></span>
     </x-button>
 </div>
