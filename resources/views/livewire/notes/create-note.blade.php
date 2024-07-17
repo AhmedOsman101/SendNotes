e7c41d81f3c286dc33d8cbd5fd2611138bcd4d37
AhmadOthman <trollingosman101@outlook.com>
2024-07-17 03:35:49 +0300

    feat: create create-note.blade.php and create.blade.php

---
+++ resources/views/livewire/notes/create-note.blade.php
@@ -0,0 +1,47 @@
+<?php
+
+use App\Models\Note;
+use Livewire\Attributes\Validate;
+use Livewire\Volt\Component;
+
+new class extends Component {
+    #[Validate('required|string|min:3')]
+    public $title;
+
+    #[Validate('required|string|min:10')]
+    public $content;
+
+    #[Validate('required|email')]
+    public $recipient;
+
+    #[Validate('required|date')]
+    public $send_date;
+
+    public function submit() {
+
+        if ($this->validate()) {
+            Note::create([
+                'title' => $this->title,
+                'user_id' => Auth::id(),
+                'content' => $this->content,
+                'recipient' => $this->recipient,
+                'send_date' => $this->send_date,
+            ]);
+
+            to_route('notes.index');
+        }
+    }
+}; ?>
+
+<div>
+    <form class="space-y-4" wire:submit='submit'>
+        <x-input label="Note Title" placeholder="It's been a great day." wire:model="title" />
+        <x-textarea label="Your Note" placeholder="Share all your thoughts with your friend." wire:model="content" />
+        <x-input icon="user" label="Recipient" placeholder="yourfriend@email.com" type="email" wire:model="recipient" />
+        <x-input icon="calendar" label="Send Date" type="date" wire:model="send_date" />
+        <div class="pt-4">
+            <x-button primary right-icon="calendar" spinner type="submit">Schedule Note</x-button>
+        </div>
+        <x-errors />
+    </form>
+</div>
---
+++ resources/views/notes/create.blade.php
@@ -0,0 +1,14 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="text-xl font-semibold leading-tight text-gray-800">
+            {{ __('Create a Note') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="mx-auto max-w-2xl space-y-4 sm:px-6 lg:px-8">
+            <x-button class="mb-8" href="{{ route('notes.index') }}" icon="arrow-left">All Notes</x-button>
+            <livewire:notes.create-note />
+        </div>
+    </div>
+</x-app-layout>
