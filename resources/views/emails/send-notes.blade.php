14584e2df67d8dfe88ac7940ce16f4bca4629e07
AhmadOthman <trollingosman101@outlook.com>
2024-07-19 20:52:07 +0300

    create 1 file, delete 1 file and update 1 file

--- resources/views/emails/send-notes.blade.php
+++ resources/views/emails/send-notes.blade.php
@@ -1,18 +1,111 @@
 <!DOCTYPE html>
-<html lang="en">
-
-    <head>
-        <meta charset="UTF-8">
-        <meta content="width=device-width, initial-scale=1.0" name="viewport">
-        <meta content="ie=edge" http-equiv="X-UA-Compatible">
-        <title>Document</title>
-    </head>
-
-    <body>
-        <h1>
-            Hello, you've received a new note. View it <a href="{{ $noteUrl }}" rel="noopener noreferrer"
-                target="_blank">here</a>
-        </h1>
-    </body>
-
+<html lang="en" xmlns:v="urn:schemas-microsoft-com:vml">
+<head>
+  <meta charset="utf-8">
+  <meta name="x-apple-disable-message-reformatting">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="format-detection" content="telephone=no, date=no, address=no, email=no, url=no">
+  <meta name="color-scheme" content="light dark">
+  <meta name="supported-color-schemes" content="light dark">
+  <!--[if mso]>
+  <noscript>
+    <xml>
+      <o:OfficeDocumentSettings xmlns:o="urn:schemas-microsoft-com:office:office">
+        <o:PixelsPerInch>96</o:PixelsPerInch>
+      </o:OfficeDocumentSettings>
+    </xml>
+  </noscript>
+  <style>
+    td,th,div,p,a,h1,h2,h3,h4,h5,h6 {font-family: "Segoe UI", sans-serif; mso-line-height-rule: exactly;}
+  </style>
+  <![endif]-->
+  <title>New Note Notification</title>
+  <style>
+    img {
+      max-width: 100%;
+      vertical-align: middle
+    }
+    @media (max-width: 600px) {
+      .sm-my-8 {
+        margin-top: 32px !important;
+        margin-bottom: 32px !important
+      }
+      .sm-px-4 {
+        padding-left: 16px !important;
+        padding-right: 16px !important
+      }
+      .sm-px-6 {
+        padding-left: 24px !important;
+        padding-right: 24px !important
+      }
+      .sm-leading-8 {
+        line-height: 32px !important
+      }
+    }
+  </style>
+</head>
+<body style="margin: 0; width: 100%; background-color: #030712; padding: 0; -webkit-font-smoothing: antialiased; word-break: break-word">
+  <div role="article" aria-roledescription="email" aria-label="New Note Notification" lang="en">
+    <div class="sm-px-4" style="background-color: #030712; font-family: ui-sans-serif, system-ui, -apple-system, 'Segoe UI', sans-serif; color: #f3f4f6">
+      <table align="center" cellpadding="0" cellspacing="0" role="none">
+        <tr>
+          <td style="width: 552px; max-width: 100%">
+            <div class="sm-my-8" style="margin-top: 48px; margin-bottom: 48px; text-align: center">
+              <a href="{{env('APP_URL')}}">
+                <svg fill="none" viewBox="0 0 448 470" xmlns="http://www.w3.org/2000/svg" style="height: 80px">
+                  <path d="M407.776 435v12.5c0 4.136 3.364 7.5 7.5 7.5h10c4.136 0 7.5-3.364 7.5-7.5V435h-25Z" fill="#F53635"></path>
+                  <path d="M432.776 135.813h-25v284.188h25V135.813Z" fill="#FAB43E"></path>
+                  <path d="M411.517 120.813h17.519l-8.76-21.071-8.759 21.071ZM125.224 160h155.105V85H125.224v75Zm22.5-60h110.105a7.5 7.5 0 0 1 7.5 7.5 7.5 7.5 0 0 1-7.5 7.5H147.724a7.5 7.5 0 0 1-7.5-7.5 7.5 7.5 0 0 1 7.5-7.5Zm0 30h110.105a7.5 7.5 0 0 1 7.5 7.5 7.5 7.5 0 0 1-7.5 7.5H147.724a7.5 7.5 0 0 1-7.5-7.5 7.5 7.5 0 0 1 7.5-7.5Z" fill="#FDF3C2"></path>
+                  <path d="M47.724 440a7.5 7.5 0 0 1-7.5-7.5v-60a7.5 7.5 0 0 1 7.5-7.5c.003 0 .003-15 .003-15a7.5 7.5 0 0 1-7.503-7.5v-305a7.5 7.5 0 0 1 7.5-7.5c.003 0 .003-15 .003-15H15.224v440h32.503s-.001-15-.003-15Z" fill="#C40200"></path>
+                  <path d="M327.829 15H47.726v15a7.5 7.5 0 0 1 7.497 7.5v305a7.5 7.5 0 0 1-7.497 7.5v15a7.5 7.5 0 0 1 7.497 7.5v60a7.5 7.5 0 0 1-7.497 7.5v15h280.103c12.407 0 22.5-10.094 22.5-22.5v-395c0-12.406-10.093-22.5-22.5-22.5Zm-32.5 152.5a7.5 7.5 0 0 1-7.5 7.5H117.724a7.5 7.5 0 0 1-7.5-7.5v-90a7.5 7.5 0 0 1 7.5-7.5h170.105a7.5 7.5 0 0 1 7.5 7.5v90Z" fill="#F53635"></path>
+                  <path d="M327.829 0H7.724a7.5 7.5 0 0 0-7.5 7.5v455a7.5 7.5 0 0 0 7.5 7.5h320.105c20.678 0 37.5-16.822 37.5-37.5v-395c0-20.678-16.822-37.5-37.5-37.5Zm22.5 432.5c0 12.406-10.093 22.5-22.5 22.5H15.224V15h312.605c12.407 0 22.5 10.094 22.5 22.5v395Z" fill="#570100"></path>
+                  <path d="M40.224 37.5v305a7.5 7.5 0 0 0 15 0v-305a7.5 7.5 0 0 0-15 0ZM40.224 372.5v60a7.5 7.5 0 0 0 15 0v-60a7.5 7.5 0 0 0-15 0ZM287.829 70H117.724a7.5 7.5 0 0 0-7.5 7.5v90a7.5 7.5 0 0 0 7.5 7.5h170.105a7.5 7.5 0 0 0 7.5-7.5v-90a7.5 7.5 0 0 0-7.5-7.5Zm-7.5 90H125.224V85h155.105v75Z" fill="#570100"></path>
+                  <path d="M147.724 115h110.105a7.5 7.5 0 0 0 7.5-7.5 7.5 7.5 0 0 0-7.5-7.5H147.724a7.5 7.5 0 0 0-7.5 7.5 7.5 7.5 0 0 0 7.5 7.5ZM147.724 145h110.105a7.5 7.5 0 0 0 7.5-7.5 7.5 7.5 0 0 0-7.5-7.5H147.724a7.5 7.5 0 0 0-7.5 7.5 7.5 7.5 0 0 0 7.5 7.5ZM447.739 127.566c-.018-.184-.05-.365-.081-.545-.011-.061-.016-.122-.028-.183a7.624 7.624 0 0 0-.166-.662l-.013-.05a7.458 7.458 0 0 0-.28-.767l-19.969-48.034a7.5 7.5 0 0 0-13.85 0l-19.969 48.034c-.107.249-.2.505-.28.767l-.013.05a7.624 7.624 0 0 0-.166.662c-.012.061-.017.122-.028.183-.031.18-.063.361-.081.545a7.443 7.443 0 0 0-.038.747V447.5c0 12.406 10.093 22.5 22.5 22.5h10c12.407 0 22.5-10.094 22.5-22.5V128.313a8.07 8.07 0 0 0-.038-.747ZM425.276 455h-10c-4.136 0-7.5-3.364-7.5-7.5V435h25v12.5c0 4.136-3.364 7.5-7.5 7.5Zm7.5-319.187V420h-25V135.813h25Zm-21.259-15 8.76-21.07 8.76 21.07h-17.52Z" fill="#570100"></path>
+                </svg>
+              </a>
+            </div>
+            <table style="width: 100%;" cellpadding="0" cellspacing="0" role="none">
+              <tr>
+                <td class="sm-px-6" style="border-radius: 4px; background-color: #334155; padding: 48px; font-size: 16px; color: #fffffe; box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05)">
+                  <h1 class="sm-leading-8" style="margin: 0 0 24px; font-size: 24px; font-weight: 600">
+                    Hello,
+                  </h1>
+                  <p style="margin: 0; line-height: 24px">
+                    You've received a new note from {{$note->user->name}}.
+                  </p>
+                  <p>View it by clicking the button below:</p>
+                  <div role="separator" style="line-height: 5px">&zwj;</div>
+                  <div>
+                    <a href="{{$noteUrl}}" style="color: #f8fafc; background-color: #4338ca; display: inline-block; border-radius: 4px; padding: 16px 24px; font-size: 16px; line-height: 1; font-weight: 600; text-decoration: none">
+                      <!--[if mso]>
+      <i style="mso-font-width: 150%; mso-text-raise: 30px" hidden>&emsp;</i>
+    <![endif]-->
+                      <span style="mso-text-raise: 16px">
+                  View Note &rarr;
+                </span>
+                      <!--[if mso]>
+      <i hidden style="mso-font-width: 150%;">&emsp;&#8203;</i>
+    <![endif]-->
+                    </a>
+                  </div>
+                  <div role="separator" style="height: 1px; line-height: 1px; margin: 25px 0; background-color: #e2e8f0">&zwj;</div>
+                  <p style="margin: 0;">
+                    If you didn't sign up for {{env('APP_NAME')}}, you can safely
+                    ignore this email.
+                    <br>
+                    <br>
+                    Thanks, <br>The {{env('APP_NAME')}}'s Team
+                  </p>
+                </td>
+              </tr>
+              <tr role="separator">
+                <td style="line-height: 48px">&zwj;</td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+      </table>
+    </div>
+  </div>
+</body>
 </html>
